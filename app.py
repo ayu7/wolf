f777d790ba3fe2373f72269291fc8d8fb434eabd
JFreyra <efreyra@stuy.edu>
2017-05-15 00:05:13 -0400

    basic redirection in app.py; API call and return in mathpix.py; matrix/vector conversion in mathpix.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for, request, redirect
+#from utils import mathpix, math
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/draw")
+def draw():
+    return render_template("draw.html")
+
+@app.route("/type")
+def type():
+    return render_template("type.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+# Turn off before release
+if __name__ == "__main__":
+    app.run( debug=True );
---
+++ utils/mathpix.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import base64
+import requests
+import json
+
+# submits a query to Mathpix
+# takes argument image which is a filepath
+def query(image):
+    image_uri = "data:image/jpg;base64," + base64.b64encode(open(image, "rb").read())
+    r = requests.post("https://api.mathpix.com/v3/latex",
+                      data=json.dumps({'url': image_uri}),
+                      headers={"app_id": "efrey", "app_key": "5f578f9e0320da38afcf226cd61b6513",
+                               "Content-type": "application/json"})
+    return json.dumps(json.loads(r.text), indent=4, sort_keys=True)
+
+# Checks/Returns if 'a' is convertible to int
+def isInt(a):
+    try:
+        int(a)
+        return True
+    except:
+        return False
+
+def matrixConvert(latex):
+    # example matrix
+    # \\left[ \\begin{array} { l l } { 1} & { 0} \\\\ { 0} & { 1} \\end{array} \\right]
+    # example vector
+    # \\left[ \\begin{array} { l l l } { 1} & { 2} & { 3} \\end{array} \\right]
+    
+    # if contains '\\\\': matrix else vector
+
+    if r"\\\\" not in latex:
+        return [int(x) for x in latex if isInt(x)] 
+    else:
+        split = latex.split(r"\\\\")
+        return [[int(x) for x in i if isInt(x)] for i in latex.split(r"\\\\")]
+
+    
+def latexConvert(latex):
+    parsed = ""
+    return parsed
