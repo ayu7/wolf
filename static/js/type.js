55318ac034f21298ece92d7545555e291e879d2c
ayu7 <ayu7@stuy.edu>
2017-05-26 13:59:23 -0400

    js for frontend changes

--- static/js/type.js
+++ static/js/type.js
@@ -1,10 +1,12 @@
 var label= document.getElementById("label");
 var input= document.getElementById("input2");
+var div= document.getElementById("col1");
 
 //hides second input field & the text
 var hide=function(){
   label.setAttribute("hidden", true);
   input.setAttribute("hidden", true);
+  div.setAttribute("class", "col-lg-6 col-lg-offset-3");
   console.log("hide");
 };
 
@@ -12,6 +14,7 @@ var hide=function(){
 var show=function(){
   label.removeAttribute("hidden");
   input.removeAttribute("hidden");
+  div.setAttribute("class", "col-lg-6");
   console.log("show");
 };
 
